ba2c4e5211e98be519727a280396c6075fe6f1d0
spheleletholy262000@gmail.com <spheleletholy262000@gmail.com@users.noreply.github.com>
2022-05-16 03:34:32 +0200

    PDF to JPEG Complete :)

--- Controllers/Convert_PDF_To_JPEGController.cs
+++ Controllers/Convert_PDF_To_JPEGController.cs
@@ -11,14 +11,31 @@ namespace ImageCompressionAPI.Controllers
     [ApiController]
     public class Convert_PDF_To_JPEGController : ControllerBase
     {
-
+       
         [HttpGet]
         public IActionResult Convert()
         {
 
-            string inputPdfPath = @"C:\Users\User\Downloads\Images\AccountDetails (1).pdf";
+            string inputPdfPath = @"C:\Output-PDFs\1- converted.pdf";
             string outputPath = @"C:\Output-PDFs\Output images";
-            int PageNumber;
+            int PageNumber = 1;
+
+            string outImageName = Path.GetFileNameWithoutExtension(inputPdfPath);
+            outImageName = outImageName + "_" + PageNumber.ToString() + "_.png";
+
+
+            GhostscriptPngDevice dev = new GhostscriptPngDevice(GhostscriptPngDeviceType.Png256);
+            dev.GraphicsAlphaBits = GhostscriptImageDeviceAlphaBits.V_4;
+            dev.TextAlphaBits = GhostscriptImageDeviceAlphaBits.V_4;
+            dev.ResolutionXY = new GhostscriptImageDeviceResolution(290, 290);
+            dev.InputFiles.Add(inputPdfPath);
+            dev.Pdf.FirstPage = PageNumber;
+            dev.Pdf.LastPage = PageNumber;
+            dev.CustomSwitches.Add("-dDOINTERPOLATE");
+           // dev.OutputPath = Server.MapPath(@"~/tempImages/" + outImageName);
+            dev.OutputPath = Path.Combine(outputPath + outImageName);
+            dev.Process();
+
 
 
             return Ok();

5c580ac48662c3be81f4fb6d4bd76645ef74a71e
spheleletholy262000@gmail.com <spheleletholy262000@gmail.com@users.noreply.github.com>
2022-05-16 03:44:18 +0200

    Added the Compress controller

---
+++ Controllers/CompressController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImageCompressionAPI.Controllers
+{
+    public class CompressController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

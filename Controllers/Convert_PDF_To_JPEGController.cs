917cf6086df676f4c7933780957ac89cdcd20b72
spheleletholy262000@gmail.com <spheleletholy262000@gmail.com@users.noreply.github.com>
2022-05-17 14:27:49 +0200

    Added the a Foreach loop to get all the PDFs

--- Controllers/Convert_PDF_To_JPEGController.cs
+++ Controllers/Convert_PDF_To_JPEGController.cs
@@ -16,25 +16,33 @@ namespace ImageCompressionAPI.Controllers
         public IActionResult Convert()
         {
 
-            string inputPdfPath = @"C:\Output-PDFs\1- converted.pdf";
+            //string inputPdfPath = @"C:\Output-PDFs\";
             string outputPath = @"C:\Output-PDFs\Output images";
+            string[] inputPdfPath = Directory.GetFiles(@"C:\Output-PDFs", "*.pdf");
             int PageNumber = 1;
 
-            string outImageName = Path.GetFileNameWithoutExtension(inputPdfPath);
-            outImageName = outImageName + "_" + PageNumber.ToString() + "_.png";
+   
+            foreach (var item in inputPdfPath)
+            {
+                string outImageName = Path.GetFileNameWithoutExtension(item);
+                outImageName = outImageName + "_" + PageNumber.ToString() + "_.png";
 
 
-            GhostscriptPngDevice dev = new GhostscriptPngDevice(GhostscriptPngDeviceType.Png256);
-            dev.GraphicsAlphaBits = GhostscriptImageDeviceAlphaBits.V_4;
-            dev.TextAlphaBits = GhostscriptImageDeviceAlphaBits.V_4;
-            dev.ResolutionXY = new GhostscriptImageDeviceResolution(290, 290);
-            dev.InputFiles.Add(inputPdfPath);
-            dev.Pdf.FirstPage = PageNumber;
-            dev.Pdf.LastPage = PageNumber;
-            dev.CustomSwitches.Add("-dDOINTERPOLATE");
-           // dev.OutputPath = Server.MapPath(@"~/tempImages/" + outImageName);
-            dev.OutputPath = Path.Combine(outputPath + outImageName);
-            dev.Process();
+                GhostscriptPngDevice dev = new GhostscriptPngDevice(GhostscriptPngDeviceType.Png256);
+                dev.GraphicsAlphaBits = GhostscriptImageDeviceAlphaBits.V_4;
+                dev.TextAlphaBits = GhostscriptImageDeviceAlphaBits.V_4;
+                dev.ResolutionXY = new GhostscriptImageDeviceResolution(290, 290);
+                dev.InputFiles.Add(item);
+                dev.Pdf.FirstPage = PageNumber;
+                dev.Pdf.LastPage = PageNumber;
+                dev.CustomSwitches.Add("-dDOINTERPOLATE");
+            
+                // dev.OutputPath = Server.MapPath(@"~/tempImages/" + outImageName);
+                dev.OutputPath = Path.Combine(outputPath, outImageName);
+                dev.Process();
+            }
+
+
 
 
 
